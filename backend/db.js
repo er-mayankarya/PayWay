89d08b6a5ffc70ef22d661844e61402aa393df77
Mayank Arya <ermayankarya7@gmail.com>
2025-04-27 18:44:32 +0530

    User Schema updated

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema } from "mongoose";
 
 const userSchema = new Schema({
-    email : String ,
+    username : String ,
     password : String ,
     firstName : String ,
     lastName : String 
