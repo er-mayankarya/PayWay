749a808053cde2abb9df5d9b488d316267f1207a
Mayank Arya <ermayankarya7@gmail.com>
2025-04-28 00:10:05 +0530

    Users Schema Updated

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,28 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, Types } from "mongoose";
 
 const userSchema = new Schema({
-    username : String ,
-    password : String ,
-    firstName : String ,
-    lastName : String 
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    minLength: 3,
+    maxLength: 30,
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6,
+  },
+  firstName: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    maxLength: 50,
+  },
 });
 
-export const userModel = mongoose.model("User" , userSchema);
+export const userModel = mongoose.model("User", userSchema);
