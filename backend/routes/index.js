a3462cae71aa067385adfcf76a6df17ce15ddcbb
Mayank Arya <ermayankarya7@gmail.com>
2025-04-29 08:25:41 +0530

    account Rounting added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 import express, { Router } from "express";
 import { userRouter } from "./user.js";
+import { accountRouter } from "./account.js";
 
 export const rootRouter = Router();
 
 rootRouter.use("/user" , userRouter);
+rootRouter.use("/account" , accountRouter);
